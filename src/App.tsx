9ae4ae95cb927373672bdbf686c6ef784c075551
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-14 12:37:51 -0500

    Configuracion directorios

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { NavigationContainer } from '@react-navigation/native'
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>
+    Hello World
+      </Text>
+    </NavigationContainer>
+  )
+}
